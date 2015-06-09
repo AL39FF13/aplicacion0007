ba5eae580bdc70300993b50fc3caa821befcb38e
AL39FF13 <nina141914@gmail.com>
2015-06-09 14:08:50 -0500

    seis

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	
+	audio = window.plugins.LowLatencyAudio;
+	audio.preloadFX ('B1','audio/C.mp3',function(){}, function(msg){ alert ("Error " + msg);});
+	
+	audio.preloadFX ('B2','audio/D.mp3',function(){}, function(msg){ alert ("Error " + msg);});
+	
+	audio.preloadFX ('B3','audio/E.mp3',function(){}, function(msg){ alert ("Error " + msg);});
+	
+	audio.preloadFX ('B4','audio/F.mp3',function(){}, function(msg){ alert ("Error " + msg);});
+	
 	var basedatos=window.sqlitePlugin.opendDatabase({name: "ColoresBD.db", createFromLocation: 1});
 	
 	cargarnombrejugador();
@@ -19,15 +29,7 @@ document.addEventListener("deviceready",function(){
 }
 	
 	
-audio = window.plugins.LowLatencyAudio;
-	audio.preloadFX ('B1','audio/C.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	
-	audio.preloadFX ('B2','audio/D.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	
-	audio.preloadFX ('B3','audio/E.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	
-	audio.preloadFX ('B4','audio/F.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	
+
 $('#btnjugar').on ('tap',function(){
 	var pantalla=$.mobile.getScreenHeight();
 	var encabezado=$('.ui-header').outerHeight();
